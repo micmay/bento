eb3f11973cf83748210600618d04edb558dae96f
Michael Mayer <2beorn2be@gmail.com>
2018-06-18 22:18:34 +0200

    add box once build

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ packer build -only=virtualbox-iso -var 'name=fwdev' debian-9.4-amd64.json
 popd
 
 pushd builds
-#vagrant box add fwdev.virtualbox.box  --name fwdev
+vagrant box add fwdev.virtualbox.box  --name fwdev
 popd
