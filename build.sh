4427a870881bd1bee1973555df43dd819fa2da7b
Michael Mayer <2beorn2be@gmail.com>
2018-06-18 21:43:21 +0200

    executable scripts

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ packer build -only=virtualbox-iso debian-9.4-amd64.json -var 'name=fwdev'
 popd
 
 pushd builds
-vagrant box add fwdev.virtualbox.box  --name fwdev
+#vagrant box add fwdev.virtualbox.box  --name fwdev
 popd
